56da3c36e62c262936ec4964b320c2e328cb8538
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-08-31 14:46:43 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtente
+{
+    public class Utente
+    {
+        public static string Username { get; private set; }
+        private static string Password { get; set; }
+        private static DateTime LastLogin
+        {
+            get; set;
+        }
+        private static List<DateTime> Accessi { get; } = new List<DateTime>();
+        public static bool IsLogged => !string.IsNullOrEmpty(Username);
+
+        public static void Login()
+        {
+            Console.Write("Inserisci username: ");
+            string inputUsername = Console.ReadLine();
+
+            Console.Write("Inserisci password: ");
+            string inputPassword = Console.ReadLine();
+
+            Console.Write("Conferma password: ");
+            string confirmPassword = Console.ReadLine();
+
+            if (inputUsername == "" || inputPassword != confirmPassword)
+            {
+                Console.WriteLine("Login fallito. Username vuota o password non coincidenti.");
+            }
+            else
+            {
+                Username = inputUsername;
+                Password = inputPassword;
+                LastLogin = DateTime.Now;
+                Accessi.Add(LastLogin);
+                Console.WriteLine("Login effettuato con successo.");
+            }
+        }
+        public static void Logout()
+        {
+            if (IsLogged)
+            {
+                Username = null;
+                Password = null;
+                Console.WriteLine("Logout effettuato.");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato.");
+            }
+        }
+
+        public static void VerificaOraDataLogin()
+        {
+            if (IsLogged)
+            {
+                Console.WriteLine($"Ultimo login effettuato il: {LastLogin}");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato.");
+            }
+        }
+
+        public static void ListaAccessi()
+        {
+            if (IsLogged)
+            {
+                Console.WriteLine("Accessi precedenti:");
+                foreach (DateTime accesso in Accessi)
+                {
+                    Console.WriteLine(accesso);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato.");
+            }
+        }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1.: Login");
+                Console.WriteLine("2.: Logout");
+                Console.WriteLine("3.: Verifica ora e data di login");
+                Console.WriteLine("4.: Lista degli accessi");
+                Console.WriteLine("5.: Esci");
+                Console.WriteLine("========================================");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Utente.Login();
+                        break;
+                    case 2:
+                        Utente.Logout();
+                        break;
+                    case 3:
+                        Utente.VerificaOraDataLogin();
+                        break;
+                    case 4:
+                        Utente.ListaAccessi();
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida.");
+                        break;
+                }
+            }
+            }
+    }
+}
